41cd0c41eea1e3b1736bd3da20b8ea8f5163e625
Cooper Ry Lees <me@cooperlees.com>
2018-01-07 21:54:05 -0800

    Simple example of listening on IPv4 + IPv6

--- vps/main.py
+++ vps/main.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
+import asyncio
 import click
 import logging
+import socket
 
 from aiohttp import web
 from typing import Any, Union
@@ -14,32 +16,57 @@ async def hello(request: Any) -> web.Response:
     return web.Response(text="Hello, world")
 
 
-def _handle_debug(ctx, param, debug) -> Union[bool, int, str]:
+def _handle_debug(
+    ctx: click.core.Context,
+    param: Union[click.core.Option, click.core.Parameter],
+    debug: Union[bool, int, str],
+) -> Union[bool, int, str]:
     '''Turn on debugging if asked otherwise INFO default'''
     log_level = logging.DEBUG if debug else logging.INFO
     logging.basicConfig(
-        format=(
-            '[%(asctime)s] %(levelname)s: %(message)s ' +
-            '(%(filename)s:%(lineno)d)'
-        ),
-        level=log_level
+        format=('[%(asctime)s] %(levelname)s: %(message)s ' +
+                '(%(filename)s:%(lineno)d)'),
+        level=log_level,
     )
     return debug
 
 
+async def async_main(
+    debug: bool,
+    port: int,
+) -> None:
+    loop = asyncio.get_event_loop()
+    server = web.Server(hello)
+    sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
+    sock.bind(('::', port))
+    try:
+        http_server = await loop.create_server(server, sock=sock)
+        LOG.info(f"Listenening on [::]:{port} ...")
+        await asyncio.sleep(2**64)  # COOPER - Fix to wait nicely on server
+    finally:
+        http_server.close()
+        await http_server.wait_closed()
+
+
 @click.command()
+@click.option('--debug', is_flag=True, callback=_handle_debug,
+              show_default=True, help='Turn on debug logging')
 @click.option(
     '-p', '--port', default=80, help='TCP port to listen on for HTTP',
 )
 @click.pass_context
 def main(
     ctx: click.Context,
+    debug: bool,
     port: int,
 ) -> None:
-    LOG.info
-    app = web.Application()
-    app.router.add_get('/', hello)
-    web.run_app(app, port=port)
+    loop = asyncio.get_event_loop()
+    try:
+        loop.run_until_complete(async_main(debug, port))
+    except KeyboardInterrupt:
+        ...
+    finally:
+        loop.close()
 
 
 if __name__ == '__main__':
